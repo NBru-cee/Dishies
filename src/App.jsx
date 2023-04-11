b4432fa3b2162cf7c41c46977ec5a3219617fcfc
Bruce <nbruce420@gmail.com>
2023-04-11 23:02:46 +0200

    added app

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,42 @@
 import React from "react";
-import Form from "./Form";
-import {  Routes, Route } from 'react-router-dom'
+import {
+      BrowserRouter as Router,
+      Routes,
+      Route,
+      Navigate,
+} from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Contact from "./pages/Contact";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Menu from "./pages/Menu";
+import MenuItem from "./pages/MenuItem";
+import Profile from "./pages/Profile";
 
 const App = () => {
       return (
-            <div>
-                  <Form />
-            </div>
+            <Router>
+                  <Header />
+                  <Routes>
+                        <Route path="/" element={<Navigate to="/home" />} />
+                        <Route path="/home" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/contact" element={<Contact />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                        <Route path="/account" element={<Profile />} />
+                        <Route path="/foods" element={<Menu />} />
+                        <Route path="/foods/:id" element={<MenuItem />} />
+                        <Route path="/checkout" element={<Checkout />} />
+                        <Route path="/cart" element={<Cart />} />
+                  </Routes>
+                  <Footer />
+            </Router>
       );
 };
 
---
+++ src/assets/data/testimonials.js
@@ -0,0 +1,123 @@
+import client01 from "../imgs/testimonial-imgs/avatar-1.jpg";
+import client02 from "../imgs/testimonial-imgs/avatar-2.png";
+import client03 from "../imgs/testimonial-imgs/avatar-3.jpg";
+import client04 from "../imgs/testimonial-imgs/avatar-4.jpg";
+import client05 from "../imgs/testimonial-imgs/avatar-5.jpg";
+import client06 from "../imgs/testimonial-imgs/avatar-6.jpg";
+import client07 from "../imgs/testimonial-imgs/avatar-7.jpg";
+import client08 from "../imgs/testimonial-imgs/avatar-8.png";
+import client09 from "../imgs/testimonial-imgs/avatar-9.jpg";
+import client10 from "../imgs/testimonial-imgs/avatar-10.jpeg";
+import client11 from "../imgs/testimonial-imgs/client-1.png";
+import client12 from "../imgs/testimonial-imgs/client-2.jpg";
+import client13 from "../imgs/testimonial-imgs/client-3.jpg";
+import client14 from "../imgs/testimonial-imgs/client-4.jpg";
+import client15 from "../imgs/testimonial-imgs/client-5.jpg";
+
+export const clientData = [
+      {
+            id: 1,
+            name: "David Guetta",
+            title: "Software Engineer",
+            content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam a ut aspernatur? Sed exercitationem, aliquid numquam cupiditate a illum corporis expedita aperiam magni tempore natus esse maiores fuga voluptatem.",
+            image: client01,
+      },
+      {
+            id: 2,
+            name: "Jenna Tetrazini",
+            title: "Sports Trainer",
+            content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam a ut aspernatur? Sed exercitationem, aliquid numquam cupiditate a illum corporis expedita aperiam magni tempore natus esse maiores fuga voluptatem.",
+            image: client02,
+      },
+      {
+            id: 3,
+            name: "Victor Doe",
+            title: "Database Analyst",
+            content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam a ut aspernatur? Sed exercitationem, aliquid numquam cupiditate a illum corporis expedita aperiam magni tempore natus esse maiores fuga voluptatem.",
+            image: client03,
+      },
+      {
+            id: 4,
+            name: "Lilly Saint",
+            title: "Fashion Designer",
+            content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam a ut aspernatur? Sed exercitationem, aliquid numquam cupiditate a illum corporis expedita aperiam magni tempore natus esse maiores fuga voluptatem.",
+            image: client04,
+      },
+      {
+            id: 5,
+            name: "Amber Rose",
+            title: "Walmart Employer",
+            content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam a ut aspernatur? Sed exercitationem, aliquid numquam cupiditate a illum corporis expedita aperiam magni tempore natus esse maiores fuga voluptatem.",
+            image: client05,
+      },
+      {
+            id: 6,
+            name: "Leila Chandra",
+            title: "Movie Actor",
+            content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam a ut aspernatur? Sed exercitationem, aliquid numquam cupiditate a illum corporis expedita aperiam magni tempore natus esse maiores fuga voluptatem.",
+            image: client06,
+      },
+      {
+            id: 7,
+            name: "Bella Kaitlin",
+            title: " Psychologist",
+            content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam a ut aspernatur? Sed exercitationem, aliquid numquam cupiditate a illum corporis expedita aperiam magni tempore natus esse maiores fuga voluptatem.",
+            image: client07,
+      },
+      {
+            id: 8,
+            name: "Sussy Jane",
+            title: "Caltech Student",
+            content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam a ut aspernatur? Sed exercitationem, aliquid numquam cupiditate a illum corporis expedita aperiam magni tempore natus esse maiores fuga voluptatem.",
+            image: client08,
+      },
+      {
+            id: 9,
+            name: "Mr. Von Mcgray",
+            title: "Civil Engineer",
+            content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam a ut aspernatur? Sed exercitationem, aliquid numquam cupiditate a illum corporis expedita aperiam magni tempore natus esse maiores fuga voluptatem.",
+            image: client09,
+      },
+      {
+            id: 10,
+            name: "Chris Pratt",
+            title: "Artist &  Tech Enthusiast",
+            content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam a ut aspernatur? Sed exercitationem, aliquid numquam cupiditate a illum corporis expedita aperiam magni tempore natus esse maiores fuga voluptatem.",
+            image: client11,
+      },
+      {
+            id: 11,
+            name: "Lana Cheng",
+            title: "Caltech Student",
+            content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam a ut aspernatur? Sed exercitationem, aliquid numquam cupiditate a illum corporis expedita aperiam magni tempore natus esse maiores fuga voluptatem.",
+            image: client12,
+      },
+      {
+            id: 12,
+            name: "Sarah Jay",
+            title: "Walmart Marketing Manager",
+            content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam a ut aspernatur? Sed exercitationem, aliquid numquam cupiditate a illum corporis expedita aperiam magni tempore natus esse maiores fuga voluptatem.",
+            image: client13,
+      },
+      {
+            id: 13,
+            name: "Henry Thompson",
+            title: "Caltech Student",
+            content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam a ut aspernatur? Sed exercitationem, aliquid numquam cupiditate a illum corporis expedita aperiam magni tempore natus esse maiores fuga voluptatem.",
+            image: client14,
+      },
+      {
+            id: 14,
+            name: "Fiona Fawl",
+            title: "Fashion Model",
+            content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam a ut aspernatur? Sed exercitationem, aliquid numquam cupiditate a illum corporis expedita aperiam magni tempore natus esse maiores fuga voluptatem.",
+            image: client10,
+      },
+      {
+            id: 15,
+            name: "Oscar Hummings",
+            title: "Professional Commissioner",
+            content: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis quisquam a ut aspernatur? Sed exercitationem, aliquid numquam cupiditate a illum corporis expedita aperiam magni tempore natus esse maiores fuga voluptatem.",
+            image: client15,
+      },
+];
