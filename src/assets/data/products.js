855c332133984ca97476c6a44137f8ce43f8aa13
Bruce <nbruce420@gmail.com>
2023-04-11 19:28:30 +0200

    added images

---
+++ src/assets/data/products.js
@@ -0,0 +1,279 @@
+import burger01img01 from "../imgs/product-imgs/burger-1.jpg";
+import burger01img02 from "../imgs/product-imgs/burger-2.jpg";
+import burger01img03 from "../imgs/product-imgs/burger-3.jpg";
+import burger01img04 from "../imgs/product-imgs/burger-4.jpg";
+
+import burger02img01 from "../imgs/product-imgs/burger-5.jpg";
+import burger02img02 from "../imgs/product-imgs/burger-6.jpg";
+import burger02img03 from "../imgs/product-imgs/burger-7.jpg";
+import burger02img04 from "../imgs/product-imgs/burger-8.png";
+
+import cake01img01 from "../imgs/product-imgs/cake-1.jpg";
+import cake01img02 from "../imgs/product-imgs/cake-2.jpg";
+import cake01img03 from "../imgs/product-imgs/cake-3.png";
+import cake01img04 from "../imgs/product-imgs/cake-4.png";
+
+import chicken01img01 from "../imgs/product-imgs/chicken-1.jpg";
+import chicken01img02 from "../imgs/product-imgs/chicken-2.jpg";
+import chicken01img03 from "../imgs/product-imgs/chicken-3.jpg";
+import chicken01img04 from "../imgs/product-imgs/chicken-4.webp";
+
+import dish01img01 from "../imgs/product-imgs/dish-9.png";
+import dish01img02 from "../imgs/product-imgs/dish-2.png";
+import dish01img03 from "../imgs/product-imgs/dish-3.png";
+import dish01img04 from "../imgs/product-imgs/dish-4.png";
+
+import dish02img01 from "../imgs/product-imgs/dish-5.png";
+import dish02img02 from "../imgs/product-imgs/dish-6.png";
+import dish02img03 from "../imgs/product-imgs/dish-7.png";
+import dish02img04 from "../imgs/product-imgs/dish-8.png";
+
+import dish03img01 from "../imgs/product-imgs/extras-2.jpg";
+import dish03img02 from "../imgs/product-imgs/veggies-1.jpeg";
+import dish03img03 from "../imgs/product-imgs/veggies-2.webp";
+import dish03img04 from "../imgs/product-imgs/dish-1.webp";
+
+import pizza01img01 from "../imgs/product-imgs/pizza-1.jpg";
+import pizza01img02 from "../imgs/product-imgs/pizza-2.jpg";
+import pizza01img03 from "../imgs/product-imgs/pizza-3.jpg";
+import pizza01img04 from "../imgs/product-imgs/pizza-4.jpg";
+
+import pizza02img01 from "../imgs/product-imgs/pizza-5.jpg";
+import pizza02img02 from "../imgs/product-imgs/pizza-6.jpg";
+import pizza02img03 from "../imgs/product-imgs/pizza-7.jpg";
+import pizza02img04 from "../imgs/product-imgs/pizza-9.jpg";
+
+import pizza03img01 from "../imgs/product-imgs/pizza-8.png";
+import pizza03img02 from "../imgs/product-imgs/pizza-10.png";
+import pizza03img03 from "../imgs/product-imgs/pizza-11.png";
+import pizza03img04 from "../imgs/product-imgs/pizza-12.png";
+
+import tacos01img01 from "../imgs/product-imgs/tacos-1.jpg";
+import tacos01img02 from "../imgs/product-imgs/tacos-2.jpg";
+import tacos01img03 from "../imgs/product-imgs/tacos-3.jpg";
+import tacos01img04 from "../imgs/product-imgs/tacos-4.jpg";
+
+export const products = [
+      {
+            id: 1,
+            title: "Chicken Burger",
+            price: 24.0,
+            image01: burger01img01,
+            image02: burger01img02,
+            image03: burger01img03,
+            image04: burger01img04,
+            category: "Burger",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 2,
+            title: "Mexican Tacos",
+            price: 36.0,
+            image01: tacos01img01,
+            image02: tacos01img02,
+            image03: tacos01img03,
+            image04: tacos01img04,
+            category: "Tacos",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 3,
+            title: "Shashlik Chicken",
+            price: 70.5,
+            image01: chicken01img01,
+            image02: chicken01img02,
+            image03: chicken01img03,
+            image04: chicken01img04,
+            category: "Chicken",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 4,
+            title: " French Cake",
+            price: 43.5,
+            image01: cake01img01,
+            image02: cake01img02,
+            image03: cake01img03,
+            image04: cake01img04,
+            category: "Cake",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 5,
+            title: "Double Cheese Pizza",
+            price: 22.0,
+            image01: pizza02img01,
+            image02: pizza02img02,
+            image03: pizza02img03,
+            image04: pizza02img04,
+            category: "Pizza",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 6,
+            title: "Chicken Dish",
+            price: 64.25,
+            image01: dish03img01,
+            image02: dish03img02,
+            image03: dish03img03,
+            image04: dish03img04,
+            category: "Dish",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 7,
+            title: "Extra Mustard Burger",
+            price: 18.75,
+            image01: burger02img01,
+            image02: burger02img02,
+            image03: burger02img03,
+            image04: burger02img04,
+            category: "Burger",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 8,
+            title: "Spanish Pizza",
+            price: 36.0,
+            image01: pizza01img01,
+            image02: pizza01img02,
+            image03: pizza01img03,
+            image04: pizza01img04,
+            category: "Pizza",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 9,
+            title: "Special Veggie'",
+            price: 82.0,
+            image01: dish01img01,
+            image02: dish01img02,
+            image03: dish01img03,
+            image04: dish01img04,
+            category: "Dish",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 10,
+            title: "Pizza with Mushroom",
+            price: 49.0,
+            image01: pizza03img01,
+            image02: pizza03img02,
+            image03: pizza03img03,
+            image04: pizza03img04,
+            category: "Pizza",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 11,
+            title: "Barbaqued Chicken",
+            price: 67.0,
+            image01: chicken01img04,
+            image02: chicken01img02,
+            image03: chicken01img03,
+            image04: chicken01img01,
+            category: "Chicken",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 12,
+            title: "Chips &  Pasta Dish",
+            price: 46.0,
+            image01: dish02img01,
+            image02: dish02img02,
+            image03: dish02img03,
+            image04: dish02img04,
+            category: "Dish",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 13,
+            title: "Cheese Burger",
+            price: 27.0,
+            image01: burger02img01,
+            image02: burger02img02,
+            image03: burger01img03,
+            image04: burger01img04,
+            category: "Burger",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 14,
+            title: "Italian Green Wave",
+            price: 50.0,
+            image01: pizza01img01,
+            image02: pizza02img02,
+            image03: pizza03img03,
+            image04: pizza01img04,
+            category: "Pizza",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+
+      {
+            id: 15,
+            title: "Deluxe Burger",
+            price: 70.5,
+            image01: burger01img03,
+            image02: burger02img02,
+            image03: burger02img03,
+            image04: burger01img02,
+            category: "Burger",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 16,
+            title: " Taco Licioso",
+            price: 17.45,
+            image01: tacos01img01,
+            image02: tacos01img04,
+            image03: tacos01img02,
+            image04: tacos01img03,
+            category: "Tacos",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 17,
+            title: "Thin Cream Cake",
+            price: 33.0,
+            image01: cake01img02,
+            image02: cake01img01,
+            image03: cake01img03,
+            image04: cake01img04,
+            category: "Cake",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 18,
+            title: "Crunchy Dish",
+            price: 64.25,
+            image01: dish02img01,
+            image02: dish01img04,
+            image03: dish02img03,
+            image04: dish03img04,
+            category: "Dish",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+
+      {
+            id: 19,
+            title: "Homemade Pizza",
+            price: 59.0,
+            image01: pizza01img04,
+            image02: pizza02img02,
+            image03: pizza01img02,
+            image04: pizza03img02,
+            category: "Pizza",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+      {
+            id: 20,
+            title: " Barbaqued Special",
+            price: 44.0,
+            image01: dish02img04,
+            image02: dish03img03,
+            image03: dish02img02,
+            image04: dish01img01,
+            category: "Dish",
+            desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam nihil, nemo dolore perferendis praesentium modi, excepturi expedita maxime impedit nisi reiciendis quod asperiores aut cupiditate maiores quasi quia error voluptas",
+      },
+];
