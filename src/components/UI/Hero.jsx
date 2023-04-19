ba46086c264f897c62b4768bc1b67cfddc1c0dc8
Bruce <nbruce420@gmail.com>
2023-04-19 14:01:53 +0200

    edited newsletter

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -6,92 +6,92 @@ import { ChevronRight } from "@mui/icons-material";
 import "../../styles/home.css";
 
 const Hero = () => {
-      const settings = {
-            infinite: true,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            speed: 1000,
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            dots: true,
-            arrows: true,
-      };
+     const settings = {
+          infinite: true,
+          autoplay: true,
+          autoplaySpeed: 2000,
+          speed: 1000,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: true,
+     };
 
-      return (
-            <section style={{ padding: "1rem" }}>
-                  <Slider {...settings}>
-                        {sliderData.map((item) => (
-                              <Paper
-                                    elevation={4}
-                                    key={item.id}
-                                    sx={{
-                                          display: "flex !important",
-                                          flexDirection: {
-                                                xs: "column",
-                                                sm: "row",
-                                          },
-                                          alignItems: "center",
-                                          height: {
-                                                md: "95vh",
-                                          },
-                                          gap: "2rem",
-                                          justifyContent: "center",
-                                          padding: "1rem",
-                                          borderRadius: 0,
-                                    }}
-                              >
-                                    <Box flexBasis="50%">
-                                          <Typography
-                                                variant="h5"
-                                                color="#c91212"
-                                                mb={2}
-                                                whiteSpace="nowrap"
-                                                fontSize="clamp(2rem, 20px, 2.5rem)"
-                                                className="hero__fonts"
-                                          >
-                                                {item.title}
-                                          </Typography>
-                                          <Typography
-                                                variant="body1"
-                                                color="#14141a"
-                                                mb={2}
-                                                fontSize="clamp(1rem, 16px, 1.5rem)"
-                                                className="hero__fonts"
-                                          >
-                                                {item.desc}
-                                          </Typography>
-                                          <Stack direction="row" spacing={4}>
-                                                <Button
-                                                      variant="contained"
-                                                      color="error"
-                                                >
-                                                      Order
-                                                </Button>
-                                                <Button
-                                                      variant="outlined"
-                                                      color="error"
-                                                      endIcon={<ChevronRight />}
-                                                >
-                                                      See All
-                                                </Button>
-                                          </Stack>
-                                    </Box>
-                                    <Box flexBasis="50%">
-                                          <img
-                                                src={item.image}
-                                                alt="hero image"
-                                                style={{
-                                                      width: "100%",
-                                                      padding: "1rem",
-                                                      boxSizing: "border-box",
-                                                }}
-                                          />
-                                    </Box>
-                              </Paper>
-                        ))}
-                  </Slider>
-            </section>
-      );
+     return (
+          <section style={{ padding: "1rem" }}>
+               <Slider {...settings}>
+                    {sliderData.map((item) => (
+                         <Paper
+                              elevation={4}
+                              key={item.id}
+                              sx={{
+                                   display: "flex !important",
+                                   flexDirection: {
+                                        sm: "column",
+                                        md: "row",
+                                   },
+                                   alignItems: "center",
+                                   height: {
+                                        md: "90vh",
+                                   },
+                                   gap: "2rem",
+                                   justifyContent: "center",
+                                   padding: "1rem",
+                                   borderRadius: 0,
+                              }}
+                         >
+                              <Box flexBasis="50%">
+                                   <Typography
+                                        variant="h5"
+                                        color="#c91212"
+                                        mb={2}
+                                        whiteSpace="nowrap"
+                                        fontSize="clamp(2rem, 20px, 2.5rem)"
+                                        className="hero__fonts"
+                                   >
+                                        {item.title}
+                                   </Typography>
+                                   <Typography
+                                        variant="body1"
+                                        color="#14141a"
+                                        mb={2}
+                                        fontSize="clamp(1rem, 16px, 1.5rem)"
+                                        className="hero__fonts"
+                                   >
+                                        {item.desc}
+                                   </Typography>
+                                   <Stack direction="row" spacing={4}>
+                                        <Button
+                                             variant="contained"
+                                             color="error"
+                                        >
+                                             Order
+                                        </Button>
+                                        <Button
+                                             variant="outlined"
+                                             color="error"
+                                             endIcon={<ChevronRight />}
+                                        >
+                                             See All
+                                        </Button>
+                                   </Stack>
+                              </Box>
+                              <Box flexBasis="50%">
+                                   <img
+                                        src={item.image}
+                                        alt="hero image"
+                                        style={{
+                                             width: "100%",
+                                             padding: "1rem",
+                                             boxSizing: "border-box",
+                                        }}
+                                   />
+                              </Box>
+                         </Paper>
+                    ))}
+               </Slider>
+          </section>
+     );
 };
 
 export default Hero;
