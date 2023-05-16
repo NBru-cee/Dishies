8395a41b2d1ba3230b7818827ae264421862f3c3
Bruce <nbruce420@gmail.com>
2023-05-16 13:05:56 +0200

    added links

--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -4,91 +4,102 @@ import Slider from "react-slick";
 import { Box, Typography, Stack, Button, Paper } from "@mui/material";
 import { ChevronRight } from "@mui/icons-material";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-     const settings = {
-          infinite: true,
-          autoplay: true,
-          autoplaySpeed: 2000,
-          speed: 1000,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          dots: false,
-          arrows: false,
-     };
+      const settings = {
+            infinite: true,
+            autoplay: true,
+            autoplaySpeed: 2000,
+            speed: 1000,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: false,
+            arrows: false,
+      };
 
-     return (
-          <section style={{ padding: "2rem" }}>
-               <Slider {...settings}>
-                    {sliderData.map((item) => (
-                         <Paper
-                              elevation={4}
-                              key={item.id}
-                              sx={{
-                                   display: "flex !important",
-                                   flexDirection: {
-                                        xs: "column",
-                                        sm: "column",
-                                        md: "row",
-                                   },
-                                   height: {
-                                        xs: "160vh",
-                                        sm: "200vh",
-                                        md: "80vh",
-                                   },
-                                   alignItems: "center",
-                                   gap: "2rem",
-                                   justifyContent: "center",
-                                   padding: "1rem 2rem",
-                              }}
-                         >
-                              <Box flexBasis="50%">
-                                   <Typography
-                                        variant="h6"
-                                        color="#c91212"
-                                        mb={2}
-                                   >
-                                        {item.title}
-                                   </Typography>
-                                   <Typography
-                                        variant="subtitle2"
-                                        color="#14141a"
-                                        mb={2}
-                                   >
-                                        {item.desc}
-                                   </Typography>
-                                   <Stack direction="row" spacing={4} mt={2}>
-                                        <Button
-                                             variant="contained"
-                                             color="error"
-                                        >
-                                             Order
-                                        </Button>
-                                        <Button
-                                             variant="outlined"
-                                             color="error"
-                                             endIcon={<ChevronRight />}
-                                        >
-                                             See All
-                                        </Button>
-                                   </Stack>
-                              </Box>
-                              <Box flexBasis="50%">
-                                   <img
-                                        src={item.image}
-                                        alt="hero image"
-                                        style={{
-                                             width: "100%",
-                                             padding: "1rem",
-                                             boxSizing: "border-box",
-                                        }}
-                                   />
-                              </Box>
-                         </Paper>
-                    ))}
-               </Slider>
-          </section>
-     );
+      return (
+            <section style={{ padding: "2rem" }}>
+                  <Slider {...settings}>
+                        {sliderData.map((item) => (
+                              <Paper
+                                    elevation={4}
+                                    key={item.id}
+                                    sx={{
+                                          display: "flex !important",
+                                          flexDirection: {
+                                                xs: "column",
+                                                sm: "column",
+                                                md: "row",
+                                          },
+                                          height: {
+                                                xs: "160vh",
+                                                sm: "200vh",
+                                                md: "80vh",
+                                          },
+                                          alignItems: "center",
+                                          gap: "2rem",
+                                          justifyContent: "center",
+                                          padding: "1rem 2rem",
+                                    }}
+                              >
+                                    <Box flexBasis="50%">
+                                          <Typography
+                                                variant="h6"
+                                                color="#c91212"
+                                                mb={2}
+                                          >
+                                                {item.title}
+                                          </Typography>
+                                          <Typography
+                                                variant="subtitle2"
+                                                color="#14141a"
+                                                mb={2}
+                                          >
+                                                {item.desc}
+                                          </Typography>
+                                          <Stack
+                                                direction="row"
+                                                spacing={4}
+                                                mt={2}
+                                          >
+                                                <Link to="/checkout">
+                                                      <Button
+                                                            variant="contained"
+                                                            color="error"
+                                                      >
+                                                            Order
+                                                      </Button>
+                                                </Link>
+                                                <Link to="/foods">
+                                                      <Button
+                                                            variant="outlined"
+                                                            color="error"
+                                                            endIcon={
+                                                                  <ChevronRight />
+                                                            }
+                                                      >
+                                                            See All
+                                                      </Button>
+                                                </Link>
+                                          </Stack>
+                                    </Box>
+                                    <Box flexBasis="50%">
+                                          <img
+                                                src={item.image}
+                                                alt="hero image"
+                                                style={{
+                                                      width: "100%",
+                                                      padding: "1rem",
+                                                      boxSizing: "border-box",
+                                                }}
+                                          />
+                                    </Box>
+                              </Paper>
+                        ))}
+                  </Slider>
+            </section>
+      );
 };
 
 export default Hero;
